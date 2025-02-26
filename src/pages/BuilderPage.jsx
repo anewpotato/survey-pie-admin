a2b0cb86f7690462e48f1d1a5261b88f38fc9a23
yuseungjong <fbtmdwhd33@naver.com>
2025-02-26 15:43:43 +0900

    feature: main layout 틀 추가.

---
+++ src/pages/BuilderPage.jsx
@@ -0,0 +1,5 @@
+import MainLayout from '../layouts/MainLayout';
+
+export default function BuilderPage() {
+  return <MainLayout selectedKeys={['builder']}>builderPage</MainLayout>;
+}
---
+++ src/pages/ListPage.jsx
@@ -0,0 +1,5 @@
+import MainLayout from '../layouts/MainLayout';
+
+export default function ListPage() {
+  return <MainLayout selectedKeys={['list']}>listPage</MainLayout>;
+}
